04fdb683768c9ba6cf43d0bcc41c51ca32943b6f
codecmy <95700884+codecmy@users.noreply.github.com>
2026-08-10 13:42:29 +0530

    Fix Port config bug

--- app.py
+++ app.py
@@ -92,4 +92,4 @@ def predict_route(request:Request,file:UploadFile=File(...)):
         raise NetworkSecurityException(e,sys)
 
 if __name__ == "__main__":
-    app_run(app=app,host="localhost",port=8000)
+    app_run(app=app,host="0.0.0.0",port=8000)
