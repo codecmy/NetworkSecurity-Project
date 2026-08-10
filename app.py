5da901c4f983bea125762ac8ab8770c0b95a646e
codecmy <95700884+codecmy@users.noreply.github.com>
2026-08-10 20:44:33 +0530

    Final changes before ci/cd

--- app.py
+++ app.py
@@ -91,5 +91,6 @@ def predict_route(request:Request,file:UploadFile=File(...)):
     except Exception as e:
         raise NetworkSecurityException(e,sys)
 
+
 if __name__ == "__main__":
     app_run(app=app,host="0.0.0.0",port=8000)
